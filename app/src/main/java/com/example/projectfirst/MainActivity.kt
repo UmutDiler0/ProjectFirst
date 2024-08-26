e9746787eb2339b1b5cca525e4fc367a7f5e7299
umut <diler.2029@gmail.com>
2024-08-27 02:07:17 +0300

    bottom nav visibilty okey

--- app/src/main/java/com/example/projectfirst/MainActivity.kt
+++ app/src/main/java/com/example/projectfirst/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.projectfirst
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -8,11 +10,16 @@ import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.example.projectfirst.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding : ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
     lateinit var navHostFragment: NavHostFragment
+    lateinit var bottomNav: BottomNavigationView
+    var menu: Menu? = null
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,16 +27,29 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         initViews()
 
+        bottomNav = binding.bottomNav
+
+        menu?.findItem(R.id.hapinessFragment)?.isVisible = false
+
     }
 
-    fun initViews(){
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.bottom_nav_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    fun initViews() {
         enableEdgeToEdge()
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragmentView) as NavHostFragment
+        navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.navHostFragmentView) as NavHostFragment
         binding.bottomNav.setupWithNavController(navHostFragment.navController)
     }
+
+
+
 }
